3d9c368da8fd42d994772c54222733d406948440
mateus-sartorio <mateussartorio39@gmail.com>
2025-07-24 20:24:34 -0300

    feat: configure BLE device configuration

--- lib/screens/bluetooth_connection_screen.dart
+++ lib/screens/bluetooth_connection_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io'; // Importado para verificar a plataforma (Android/iOS)
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:mao_robotica_app/constants.dart';
 import 'package:permission_handler/permission_handler.dart';
 import '../services/bluetooth_service.dart';
 import 'dart:async';
@@ -11,32 +12,92 @@ class BluetoothConnectionScreen extends StatefulWidget {
   const BluetoothConnectionScreen({super.key, required this.service});
 
   @override
-  State<BluetoothConnectionScreen> createState() =>
-      _BluetoothConnectionScreenState();
+  State<BluetoothConnectionScreen> createState() => _BluetoothConnectionScreenState();
 }
 
 class _BluetoothConnectionScreenState extends State<BluetoothConnectionScreen> {
-  BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
-  late StreamSubscription<BluetoothAdapterState> _adapterStateStateSubscription;
+  final Guid serviceUuid = Guid("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
+  final Guid characteristicUuid = Guid("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+  late BluetoothDevice selectedDevice;
 
   @override
   void initState() {
     super.initState();
 
-    _adapterStateStateSubscription = FlutterBluePlus.adapterState.listen((state) {
-      _adapterState = state;
-      FlutterBluePlus.scanResults.listen((results){
-        print(results);
-      });
+    FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
+      if (state == BluetoothAdapterState.on) {
+        FlutterBluePlus.onScanResults.listen((results) {
+          if (results.isNotEmpty) {
+            var result = results.first;
+            var device = result.device;
+            selectedDevice = device;
+            device.connectionState.listen((BluetoothConnectionState state) async {
+              if (state == BluetoothConnectionState.disconnected) {
+                // 1. typically, start a periodic timer that tries to
+                //    reconnect, or just call connect() again right now
+                // 2. you must always re-discover services after disconnection!
+                print("${device.disconnectReason?.code} ${device.disconnectReason?.description}");
+              }
+            });
+
+            connectToDevice(device);
+          }
+        },
+          onError: (e) => print(e),
+        );
+      } else {
+        // show an error to the user, etc
+      }
     });
+  }
+
+  Future<void> test() async {
+    await FlutterBluePlus.startScan(
+        withServices: [serviceUuid],
+        timeout: Duration(seconds:15)
+    );
+  }
+
+  Future<void> connectToDevice(BluetoothDevice device) async {
+    try {
+      await device.connect(timeout: const Duration(seconds: 10));
+      print("Connected to device: ${device.name}");
+      sendMessage();
+    } catch (e) {
+      print("Error connecting: $e");
+    }
   }
 
   @override
   void dispose() {
     // Para a busca ao sair da tela para economizar recursos e bateria.
     widget.service.stopScan();
-    _adapterStateStateSubscription.cancel();
     super.dispose();
+  }
+
+  void sendMessage() async {
+    // Discover services
+    List<BluetoothService> services = await selectedDevice.discoverServices();
+    for (var service in services) {
+      if (service.uuid == serviceUuid) {
+        print("Found service: ${service.uuid}");
+
+        // Look for characteristic
+        for (var characteristic in service.characteristics) {
+
+          var message = "Hello World!";
+          if (characteristic.uuid == characteristicUuid) {
+            print("Found characteristic, writing...");
+
+            // Convert string to bytes and write
+            List<int> bytes = message.codeUnits; // UTF-8 by default
+            await characteristic.write(bytes, withoutResponse: false);
+
+            print("Message written: $message");
+          }
+        }
+      }
+    }
   }
 
   Future<void> _requestPermissions() async {
@@ -56,7 +117,11 @@ class _BluetoothConnectionScreenState extends State<BluetoothConnectionScreen> {
         actions: [
           IconButton(
             icon: const Icon(Icons.refresh),
-            onPressed: () => {},
+            onPressed: test,
+          ),
+          IconButton(
+            icon: const Icon(Icons.send),
+            onPressed: () => sendMessage(),
           )
         ],
       ),
