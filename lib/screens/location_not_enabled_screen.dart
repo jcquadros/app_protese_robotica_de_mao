7b2b0390250e38e32bd6550045964280a4faba05
jcquadros <julliepaper@gmail.com>
2025-07-29 18:13:16 -0300

    fix: corrige bug de compatibilidade bluetooth em versoes android menores que 12

---
+++ lib/screens/location_not_enabled_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class LocationNotEnabledScreen extends StatelessWidget {
+  const LocationNotEnabledScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopScope( // Prevents user from easily swiping back if modal
+      canPop: false, // User must enable Location to proceed
+      child: Scaffold(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .surface,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.location_disabled,
+                  size: 100,
+                  color: Theme
+                      .of(context)
+                      .colorScheme
+                      .primary,
+                ),
+                const SizedBox(height: 24),
+                Text(
+                  'Localização Desativado',
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .headlineSmall
+                      ?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  'Para utilizar as funcionalidades de controle da mão robótica, por favor, ative o localização do seu dispositivo.',
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 32),
+                Text(
+                  "O aplicativo continuará assim que a localização for ativada.",
+                  style: TextStyle(fontSize: 12, color: Colors.grey[600]),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
 import flutter_blue_plus_darwin
 import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
